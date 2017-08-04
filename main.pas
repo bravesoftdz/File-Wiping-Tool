df0eddfa96cd2dcf191074f7915bf483dc4abf73
marcelohz <marcelohz@gmail.com>
2017-08-04 11:33:36 -0300

    First

--- main.pas
+++ main.pas
@@ -12,10 +12,14 @@ type
   { TMainForm }
 
   TMainForm = class(TForm)
-    Button1: TButton;
-    Button2: TButton;
+    uxOnTop: TCheckBox;
+    uxClear: TButton;
+    uxWipe: TButton;
+    uxLista: TListBox;
     procedure Button1Click(Sender: TObject);
-    procedure Button2Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormDropFiles(Sender: TObject; const FileNames: array of String);
+    procedure uxOnTopChange(Sender: TObject);
   private
     { private declarations }
   public
@@ -45,38 +49,53 @@ procedure TMainForm.Button1Click(Sender: TObject);
 begin
   //WipePath('C:\tmp\popo.pas', dmWipeZeroes);
   //WipePath('X:\oi.txt', dmWipeZeroes);
-
-
-
   //WipeFreeSpace('G', wmZeroes);
-
-
   //WipeNameAndDelete('C:\x');
-
 end;
 
-procedure TMainForm.Button2Click(Sender: TObject);
-var
-Path: String;
+procedure TMainForm.FormCreate(Sender: TObject);
 begin
+  MainForm.uxOnTopChange(nil);
+end;
 
-  WipePath('Z:\aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.txt', dmWipeZeroes);
+procedure TMainForm.FormDropFiles(Sender: TObject;
+  const FileNames: array of String);
+var
+F: String;
+i:integer;
+JahContem: boolean;
+begin
+     JahContem := false;
+     for F in FileNames do begin
+         for i := 0 to uxLista.Items.Count-1 do
+         begin
+              JahContem := false;
+              if uxLista.Items[i] = F then
+              begin
+                   JahContem := true;
+              end;
+         end;
+         if JahContem = false then
+         begin
+              uxLista.AddItem(F, nil);
+         end;
 
-  (*Path := 'X:\' + RandomName(32);
+     end;
+end;
 
-  Handle := CreateFile(PChar(Path), GENERIC_WRITE, 0, nil, CREATE_ALWAYS,
-                         FILE_FLAG_NO_BUFFERING or FILE_FLAG_WRITE_THROUGH, 0);
-  if Handle = feInvalidHandle then
-  begin
-    ShowMessage('CreateFile() failed. Last Error: ' + IntToStr(GetLastError));
-    CloseHandle(Handle);
-    Exit;
-  end;
-  CloseHandle(Handle);*)
-
+procedure TMainForm.uxOnTopChange(Sender: TObject);
+begin
+  (*if uxOnTop.Checked then SetWindowPos(MainForm.Handle, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOSIZE or SWP_NOMOVE)
+        else SetWindowPos(MainForm.Handle, HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOSIZE or SWP_NOMOVE)*)
+  if uxOnTop.Checked then
+        MainForm.FormStyle := fsStayOnTop
+  else
+        MainForm.FormStyle := fsNormal;
 
 
 end;
+
+
 
 end.
 
