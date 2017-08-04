15c280d067a77f15bbaf8899c63836555c3c1e8c
marcelohz <marcelohz@gmail.com>
2017-08-04 13:40:25 -0300

    Creating user interface.

--- main.pas
+++ main.pas
@@ -5,21 +5,35 @@ unit main;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls, wiping_tools, Windows;
+  Classes, SysUtils, FileUtil, ListViewFilterEdit, Forms, Controls, Graphics,
+  Dialogs, StdCtrls, FileCtrl, ComCtrls, wiping_tools, Windows;
 
 type
 
   { TMainForm }
 
   TMainForm = class(TForm)
+    uxWipeFreeSpace: TButton;
+    FilesDialog: TOpenDialog;
+    DirDialog: TSelectDirectoryDialog;
+    uxAddFiles: TButton;
+    uxAddDir: TButton;
+    uxView: TListView;
+    uxRemove: TButton;
+    uxRandom: TCheckBox;
     uxOnTop: TCheckBox;
     uxClear: TButton;
     uxWipe: TButton;
-    uxLista: TListBox;
     procedure Button1Click(Sender: TObject);
+    procedure FilesDialogClose(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormDropFiles(Sender: TObject; const FileNames: array of String);
+    procedure uxAddDirClick(Sender: TObject);
+    procedure uxAddFilesClick(Sender: TObject);
+    procedure uxClearClick(Sender: TObject);
     procedure uxOnTopChange(Sender: TObject);
+    procedure uxRemoveClick(Sender: TObject);
+    procedure uxWipeClick(Sender: TObject);
   private
     { private declarations }
   public
@@ -53,9 +67,20 @@ begin
   //WipeNameAndDelete('C:\x');
 end;
 
+procedure TMainForm.FilesDialogClose(Sender: TObject);
+var
+  i : integer;
+begin
+     for i := 0 to FilesDialog.Files.Count-1 do
+     begin
+          uxView.AddItem(FilesDialog.Files[i] , nil);
+     end;
+end;
+
 procedure TMainForm.FormCreate(Sender: TObject);
 begin
   MainForm.uxOnTopChange(nil);
+  FilesDialog.IntfSetOption(ofAllowMultiSelect, True);
 end;
 
 procedure TMainForm.FormDropFiles(Sender: TObject;
@@ -65,34 +90,75 @@ F: String;
 i:integer;
 JahContem: boolean;
 begin
-     JahContem := false;
      for F in FileNames do begin
-         for i := 0 to uxLista.Items.Count-1 do
+         JahContem := false;
+         for i := 0 to uxView.Items.Count-1 do
          begin
-              JahContem := false;
-              if uxLista.Items[i] = F then
+              if uxView.Items.Item[i].ToString = F then
               begin
                    JahContem := true;
               end;
          end;
          if JahContem = false then
          begin
-              uxLista.AddItem(F, nil);
+              uxView.AddItem(F, nil);
          end;
 
      end;
 end;
 
+procedure TMainForm.uxAddDirClick(Sender: TObject);
+begin
+  if DirDialog.Execute then
+  begin
+       uxView.AddItem(DirDialog.FileName, nil);
+  end;
+end;
+
+procedure TMainForm.uxAddFilesClick(Sender: TObject);
+begin
+  FilesDialog.Execute;
+
+end;
+
+procedure TMainForm.uxClearClick(Sender: TObject);
+begin
+  uxView.Clear;
+end;
+
 procedure TMainForm.uxOnTopChange(Sender: TObject);
 begin
-  (*if uxOnTop.Checked then SetWindowPos(MainForm.Handle, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOSIZE or SWP_NOMOVE)
-        else SetWindowPos(MainForm.Handle, HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOSIZE or SWP_NOMOVE)*)
-  if uxOnTop.Checked then
-        MainForm.FormStyle := fsStayOnTop
-  else
-        MainForm.FormStyle := fsNormal;
+  if uxOnTop.Checked then SetWindowPos(MainForm.Handle, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOSIZE or SWP_NOMOVE)
+        else SetWindowPos(MainForm.Handle, HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOSIZE or SWP_NOMOVE);
+  //if uxOnTop.Checked then
+  //      MainForm.FormStyle := fsStayOnTop
+  //else
+  //      MainForm.FormStyle := fsNormal;
+end;
+
+procedure TMainForm.uxRemoveClick(Sender: TObject);
+var
+i : integer;
+begin
+  while uxView.Selected <> nil do
+  begin
+       uxView.Selected.Delete;
+    end;
+  end;
 
 
+
+procedure TMainForm.uxWipeClick(Sender: TObject);
+var
+i : integer;
+Mode : TDeleteMode;
+begin
+  if uxRandom.Checked then Mode := dmWipeRandom else Mode := dmWipeZeroes;
+  for i := 0 to uxView.Items.Count-1 do
+  begin
+       WipePath(uxView.Items[i].ToString, Mode);
+  end;
+  uxView.Clear;
 end;
 
 
--- mywipingtool.lpr
+++ mywipingtool.lpr
@@ -7,7 +7,7 @@ uses
   cthreads,
   {$ENDIF}{$ENDIF}
   Interfaces, // this includes the LCL widgetset
-  Forms, main, wiping_tools
+  Forms, main, wiping_tools, lazcontrols
   { you can add units after this };
 
 {$R *.res}
